3b74a7edce916fc143bf35248491666b2550a9c1
FatHand <dontspamkball@gmail.com>
2016-09-08 21:59:39 -0400

    Suppressing ‘use var’ style warning

--- ManeuverQueue/GlobalSuppressions.cs
+++ ManeuverQueue/GlobalSuppressions.cs
@@ -7,4 +7,5 @@
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0064:Warns when a culture-aware 'string.CompareTo' call is used by default", Justification = "<Pending>")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "IDE0003:Remove 'this'", Justification = "<Pending>")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "IDE0002:Simplify member access '{0}'", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Language Usage Opportunities", "RECS0091:Use 'var' keyword when possible", Justification = "<Pending>")]
 
